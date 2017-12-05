76c626321b55652e966525eca17648c4b256efec
Adam Mohrbacher <adam.mohrbacher@gmail.com>
2017-12-05 03:02:05 +0000

    removed one binary search from address_book_spec.rb

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -58,20 +58,6 @@ require_relative '../models/address_book'
         end
     end
     
-    describe "#binary_search" do
-         it "searches AddressBook for a non-existent entry" do
-           book.import_from_csv("entries.csv")
-           entry = book.binary_search("Dan")
-           expect(entry).to be_nil
-   end
-   
-    it "searches AddressBook for Bill" do
-       book.import_from_csv("entries.csv")
-       entry = book.binary_search("Bill")
-       expect(entry).to be_a Entry
-       check_entry(entry, "Bill", "555-555-4854", "bill@blocmail.com")
-     end
-    
     describe "#import_from_csv" do
         it "imports the correct number of entries" do
            book.import_from_csv("entries.csv")
@@ -161,5 +147,4 @@ require_relative '../models/address_book'
           expect(entry).to be_nil
         end
     end 
-    end
 end
